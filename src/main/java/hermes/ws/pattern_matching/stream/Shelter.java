bb068d0f1ce82373fba0b187dd3fea157cfabf21
Cyril Carlier <cyril.carlier@hermes-ecs.com>
2020-08-26 10:17:15 +0200

    small modifications

--- src/main/java/hermes/ws/pattern_matching/stream/Shelter.java
+++ src/main/java/hermes/ws/pattern_matching/stream/Shelter.java
@@ -1,14 +1,18 @@
 package hermes.ws.pattern_matching.stream;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Shelter {
 
     private Set<Animal> animals;
 
     // example of instanceOf use with stream API
-    public void removeAggressiveAnimals(){
-        this.animals.stream().filter(a -> !(a instanceof Dog d && d.isAggressive()));
+    public void removeAggressiveAnimalsFromShelter(){
+        this.animals = this.animals.stream().filter(a -> !(a instanceof Dog d && d.isAggressive())).collect(Collectors.toSet());
     }
 
+    public void addAnimal(Animal animal) {
+        this.animals.add(animal);
+    }
 }
